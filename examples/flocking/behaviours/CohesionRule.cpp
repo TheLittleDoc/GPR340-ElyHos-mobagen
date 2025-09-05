9bfc08503086c5a0c4c3a51444816dbc74799e12
Elysium Hosack <elysium.hosack@mymail.champlain.edu>
2025-09-05 14:20:41 -0400

    Started implementing forces

--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -8,6 +8,23 @@ Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid
   // hint: iterate over the neighborhood
 
   // find center of mass
+  const Boid* thisBoid = boid;
+  Vector2f cm;
+  int count = 0;
 
-  return cohesionForce;
+  for (auto it : neighborhood) {
+    if (it == boid)
+      continue;
+    cm += thisBoid->getPosition();
+    count++;
+    }
+  if (count > 0) {
+    Vector2f Fcm = cm / count;
+    Vector2f pos = boid->getPosition();
+    Fcm -= boid->getPosition();
+    return Fcm;
+  }
+  return {0,0};
+
+
 }
